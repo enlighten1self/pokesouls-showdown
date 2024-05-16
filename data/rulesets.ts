31c71d009d8e77e2b71034876b72b6de38687641
SanjiTheLord <98240172+SanjiTheLord@users.noreply.github.com>
2024-05-16 14:09:32 -0400

    Update rulesets.ts

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -88,7 +88,7 @@ export const Rulesets: {[k: string]: FormatData} = {
 		name: 'Standard NatDex',
 		desc: "The standard ruleset for all National Dex tiers",
 		ruleset: [
-			'Obtainable', '+Unobtainable', '+Past', 'Sketch Post-Gen 7 Moves', 'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Z-Move Clause', 'Sleep Moves Clause',
+			'Obtainable', '+Unobtainable', '+Past', 'Sketch Post-Gen 7 Moves', 'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Z-Move Clause',
 		],
 		onValidateSet(set) {
 			const species = this.dex.species.get(set.species);
