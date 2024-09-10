09ce21fd712a22d58ad513a918d9c7209301f730
SanjiTheLord <98240172+SanjiTheLord@users.noreply.github.com>
2024-09-10 18:29:10 -0400

    Update formats.ts
    .

--- config/formats.ts
+++ config/formats.ts
@@ -161,36 +161,6 @@ export const Formats: FormatList = [
 		banlist: ["ND UU", "ND RUBL", "Slowbro-Base + Slowbronite", "Heat Rock"],
 	},
 	{
-		name: "[Gen 9] National Dex NU",
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3713801/">National Dex RU Metagame Discussion</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3721776/">National Dex RU Resources</a>`,
-		],
-		mod: "gen9",
-		ruleset: ["[Gen 9] National Dex RU"],
-		banlist: ["ND RU", "ND NUBL"],
-	},
-	{
-		name: "[Gen 9] National Dex NFE",
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3713801/">National Dex RU Metagame Discussion</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3721776/">National Dex RU Resources</a>`,
-		],
-		mod: "gen9",
-		ruleset: ["[Gen 9] National Dex NU"],
-		banlist: ["ND NU"],
-	},
-	{
-		name: "[Gen 9] National Dex LC",
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3713801/">National Dex RU Metagame Discussion</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3721776/">National Dex RU Resources</a>`,
-		],
-		mod: "gen9",
-		ruleset: ["[Gen 9] National Dex NFE"],
-		banlist: ["ND NFE"],
-	},
-	{
 		name: "[Gen 9] National Dex Monotype",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3710738/">National Dex Monotype Metagame Discussion</a>`,
