75609905b1e89ce477eb0e40efe0b72bca880562
SanjiTheLord <98240172+SanjiTheLord@users.noreply.github.com>
2026-09-03 02:33:15 -0400

    Update formats-data.ts

--- data/mods/retro/formats-data.ts
+++ data/mods/retro/formats-data.ts
@@ -3,4 +3,84 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		inherit: true,
 		natDexTier: "OU",
 	},
+	zacian: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	zaciancrowned: {
+		inherit: true,
+		natDexTier: "Uber",
+	},
+	baxcalibur: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	kyuremblack: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	urshifu: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	solgaleo: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	darmanitangalar: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	spectrier: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	ogerponhearthflame: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	palafin: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	zekrom: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	roaringmoon: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	fluttermane: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	starmiemega: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	dracovish: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	frostikenmega: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	ursalunabloodmoon: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	calyrexice: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	gougingfire: {
+		inherit: true,
+		natDexTier: "OU",
+	},
+	dragapult: {
+		inherit: true,
+		natDexTier: "OU",
+	},
 };
