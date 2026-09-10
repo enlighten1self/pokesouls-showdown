ecebb1cfe859a3a4578bde1fbcb90e7084f1446f
SanjiTheLord <98240172+SanjiTheLord@users.noreply.github.com>
2026-09-09 23:21:58 -0400

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -6794,7 +6794,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		baseSpecies: "Absol",
 		forme: "Mega-Z",
 		types: ["Dark", "Ghost"],
-		baseStats: {hp: 65, atk: 154, def: 60, spa: 75, spd: 60, spe: 115},
+		baseStats: {hp: 65, atk: 154, def: 60, spa: 75, spd: 60, spe: 151},
 		abilities: {0: "Sharpness"},
 		heightm: 1.2,
 		weightkg: 49,
